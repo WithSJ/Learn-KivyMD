c6de85f5fd33088a24174dd2216e96d8e2e87fdb
WithSJ <withsj.in@gmail.com>
2020-06-08 11:45:49 +0530

    Update

---
+++ tabs/1.py
@@ -0,0 +1,59 @@
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivymd.icon_definitions import md_icons
+
+KV = '''
+BoxLayout:
+    orientation: "vertical"
+
+    MDToolbar:
+        title: "Example Tabs"
+
+    MDTabs:
+        id: android_tabs
+        on_tab_switch: app.on_tab_switch(*args)
+
+
+<Tab>:
+
+    MDIconButton:
+        id: icon
+        icon: app.icons[0]
+        user_font_size: "48sp"
+        pos_hint: {"center_x": .5, "center_y": .5}
+'''
+
+
+class Tab(FloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
+
+
+class Example(MDApp):
+    icons = list(md_icons.keys())[15:30]
+
+    def build(self):
+        return Builder.load_string(KV)
+
+    def on_start(self):
+        for name_tab in self.icons:
+            self.root.ids.android_tabs.add_widget(Tab(text=name_tab))
+
+    def on_tab_switch(
+        self, instance_tabs, instance_tab, instance_tab_label, tab_text
+    ):
+        '''Called when switching tabs.
+
+        :type instance_tabs: <kivymd.uix.tab.MDTabs object>;
+        :param instance_tab: <__main__.Tab object>;
+        :param instance_tab_label: <kivymd.uix.tab.MDTabsLabel object>;
+        :param tab_text: text or name icon of tab;
+        '''
+
+        count_icon = [k for k, v in md_icons.items() if v == tab_text]
+        instance_tab.ids.icon.icon = "account"
+
+
+Example().run()
